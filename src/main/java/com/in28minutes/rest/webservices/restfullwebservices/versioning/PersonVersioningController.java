615d6f1f5579499ba78b0d9d5a8e3e7d16357892
minhtancnttptit <minhtancnttptit@gmail.com>
2020-02-20 00:08:54 +0700

    ok ok

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/versioning/PersonVersioningController.java
@@ -36,12 +36,12 @@ public class PersonVersioningController {
         return new PersonV2(new Name("Tan IT", "Minh"));
     }
 
-    @GetMapping(value = "/person/header", produces = "application/vnd.company.app-v1+json")
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
     public PersonV1 producesV1() {
         return new PersonV1("Minh Tan IT");
     }
 
-    @GetMapping(value = "/person/header", produces = "application/vnd.company.app-v2+json")
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
     public PersonV2 producesV2() {
         return new PersonV2(new Name("Tan IT", "Minh"));
     }
